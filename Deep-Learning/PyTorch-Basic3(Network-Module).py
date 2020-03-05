bdb4e7f637c2088dc6f5b10df166f18fbd432338
tak2vec <takutr9@gmail.com>
2020-03-05 22:23:05 +0900

    PyTorch Module and Nest

--- Deep-Learning/PyTorch-Basic3(Network-Module).py
+++ Deep-Learning/PyTorch-Basic3(Network-Module).py
@@ -12,35 +12,46 @@ import numpy as np
 # 独自ネットワーク層を作成することが可能
 
 # ##### Multipleクラスを作成
-# 3次元ベクトルxと同次元重みwを乗算する処理を記載</p>
-# x：[0,-1,3] | t:[0,1,2]
+# 初期値重み：torch.ones(3)<p>
+# Forward：重みと入力値xを掛け合わせた結果を返す<p>
+# 入力値x：[0,-1,3] <p>
+# ラベルt: [0,1,2]
 
-# In[11]:
+# In[17]:
 
 
 class Multiple(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.w = torch.nn.Parameter(torch.ones(3,dtype=torch.float))
+    
     def forward(self,x):
         assert x.shape == (3,)
         return x * self.w
 
 
-# In[22]:
+# In[13]:
 
 
-model = Multiple()
+from torch import optim
+from torch.nn import functional as F
+
+
+# In[25]:
+
+
 x = torch.tensor([0,-1,3],dtype=torch.float)
 t = torch.tensor([0,1,2],dtype=torch.float)
+
+model = Multiple()
+optimizer = optim.SGD(model.parameters(),lr=0.01)
 y = model(x)
-optimizer = torch.optim.SGD(model.parameters(),lr=0.01)
-loss = torch.nn.functional.mse_loss(y,t)
-print(loss)
-print()
-print(model.w.grad)
+loss = F.mse_loss(t,y)
+print(f"loss : {loss}")
+print("-------------")
 loss.backward()
-print(model.w.grad)
+print(f"重みwの勾配：{model.w.grad}")
+print("-------------")
 print(f"更新前パラメータ：{model.w}")
 optimizer.step()
 print(f"更新後パラメータ：{model.w}")
@@ -49,7 +60,7 @@ print(f"更新後パラメータ：{model.w}")
 # ##### Multipleクラスを作成②
 # 上記のクラスにさらに同次元バイアスを加算する処理を記載。モジュールのネストを実現
 
-# In[44]:
+# In[26]:
 
 
 class MultipleAndAdd(torch.nn.Module):
@@ -60,30 +71,24 @@ class MultipleAndAdd(torch.nn.Module):
     
     def forward(self,x):
         assert x.shape == (3,)
-        x = self.multiple(x)
-        return x + self.b
+        return self.multiple(x) + self.b
 
 
-# In[54]:
+# In[30]:
 
 
 model = MultipleAndAdd()
-optimizer = torch.optim.SGD(model.parameters(),lr=0.01)
-x = torch.tensor([0,-1,3],dtype=torch.float)
-t = torch.tensor([0,1,2],dtype=torch.float)
+optimizer = optim.SGD(model.parameters(),lr=0.01)
 y = model(x)
-loss = torch.nn.functional.mse_loss(t,y)
+loss = F.mse_loss(t,y)
 print(f"loss:{loss}")
+print("-----------")
 
-#Backward実施前の勾配
-print(f"現時点の重み勾配：{model.multiple.w.grad}")
-
-#Backward実施
 loss.backward()
-print(f"Backward後の重み勾配：{model.multiple.w.grad}")
-print("----------------------")
-#Parameter更新
+print(f"重みwの勾配：{model.multiple.w.grad}")
+print(f"バイアスbの勾配：{model.b.grad}")
+print("-----------")
 optimizer.step()
-print(f"重みw：{model.multiple.w}")
-print(f"バイアスb：{model.b}")
+print(f"更新後重みwパラメータ：{model.multiple.w}")
+print(f"更新後バイアスbパラメータ：{model.b}")
 
